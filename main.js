55707f5527ea26113f550da0904db9c032bbfeac
Renzo Carara <renzo.carara@libero.it>
2019-11-27 15:42:22 +0100

    completata gestione frecce avanti e indietro

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
         // nel caso sono sull'ultima allora la successiva sarà la prima in elenco(first)
         // altrimenti prendo la successiva a quella corrente
         if (!currentImg.hasClass("last")) {
-            //sto già visualizzando l'ultima immagine
+            // non sto visualizzando l'ultima immagine
             var nextImg = currentImg.next();
             console.log("nextImg:", nextImg);
 
@@ -35,9 +35,6 @@ $(document).ready(function() {
             console.log("nextImg:", nextImg);
         }
 
-
-
-
         // visualizzo l'immagine successiva aggiungendogli la classe active
         nextImg.addClass("active");
 
@@ -46,6 +43,40 @@ $(document).ready(function() {
     // intercetto click su freccia indietro
     $('#back').click(function() {
         console.log("click su back");
+
+        // l'immagine corrente è quella che ha la classe active associata
+        // in ogni istante deve essercene sempre e solo una
+        var currentImg = $('img.active');
+        console.log("currentImg:", currentImg);
+
+        // nascondo l'immagine corrente rimuovendo la classe
+        currentImg.removeClass("active");
+
+        // verifico qual è l'immagine precedente da visualizzare
+        // nel caso sono sulla prima allora la successiva sarà l'ultima in elenco (last)
+        // altrimenti prendo la precedente a quella corrente
+        if (!currentImg.hasClass("first")) {
+            // non sto visualizzando la prima immagine quindi seleziono semplicemente l'immagine precedente
+            var prevImg = currentImg.prev();
+            console.log("prevImg:", prevImg);
+
+        } else {
+            // l'immagine corrente è la prima del mio carousel, seleziono l'ultima immagine,
+            // tramite la classe last che ho associato solo all'ultima immagine
+            var prevImg = $('img.last');
+            console.log("nextImg:", prevImg);
+        }
+
+        // visualizzo l'immagine successiva aggiungendogli la classe active
+        prevImg.addClass("active");
+
+
+
+
+
+
+
+
 
     });
 
